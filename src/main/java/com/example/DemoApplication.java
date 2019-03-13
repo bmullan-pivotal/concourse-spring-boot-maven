d1ed0225728eab3d036626f7110682b5f2aa0e7f
bmullan-pivotal <bmullan@pivotal.io>
2019-03-13 10:54:57 -0400

    added println

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +17,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class DemoApplication {
 
+	static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		try {
+			throw new Exception("This is a test!");	
+		} catch(Exception ex) {
+			System.out.println("running....");
+			logger.error(ex.toString());
+		}
+
 	}
 }
 
